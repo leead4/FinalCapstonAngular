418ed99ecb40be5f977cade653fb9a7b3364673b
leead4 <leead4@gmail.com>
2017-06-19 16:26:52 -0500

    yep

--- app/app.js
+++ app/app.js
@@ -9,26 +9,16 @@ app.config(function($routeProvider){
     templateUrl: 'partials/home.html',
     controller: "HomeCtrl"
   }).
-	// when('/login', {
-	// 	templateUrl: 'partials/login.html',
-	// 	controller: "LoginCtrl"
-	// }).
-    // when('/editcard', {
-    //     templateUrl: 'partials/card.html',
-    //     controller: "CardCtrl"
-    // }).
-    when('/deck', {
+    when('/decklistview', {
+        templateUrl: 'partials/decklistview.html',
+        controller: "DeckListCtrl"
+    })
+    .when('/deck', {
       templateUrl: 'partials/deck.html',
       controller: "DeckCtrl"
     });
-    // when('/play', {
-    //   templateUrl: 'partials/play.html',
-    //   controller: "NewGardenCtrl"
-    // }).
-    // when('/home', {
-    //   templateUrl: 'partials/home.html',
-    //   controller: "HomeCtrl"
-    // });
+
+
     
 });
 
--- app/factories/cardFactory.js
+++ app/factories/cardFactory.js
@@ -4,7 +4,7 @@ app.factory("CardStorage", ($window, $q, $http) =>{
 
 let getCards = () => {
         return $q(function(resolve, reject){
-        	$http.get(`data.json`)
+        	$http.get(`http://localhost:8000/cards`)
            .then(function(cards){
                 resolve(cards);
             })
@@ -14,7 +14,46 @@ let getCards = () => {
         });
     };
 
-return {getCards};	
+    let postCard = (newCard) => {
+        return $q((resolve, reject) => {
+            $http.post(`http://localhost:8000/cards`, 
+            JSON.stringify(newCard))
+            .then((djangoStuff) => {
+                 resolve(djangoStuff);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+        });
+    };
+
+    let getDecks = () => {
+        return $q(function(resolve, reject){
+            $http.get(`http://localhost:8000/decks`)
+           .then(function(decks){
+                console.log(decks);
+                resolve(decks);
+            })
+            .catch(function(error){
+                reject(error);
+            });
+        });
+    };
+
+    let postDeck = (newDeck) => {
+        return $q((resolve, reject) => {
+            $http.post(`http://localhost:8000/decks`, 
+            JSON.stringify(newDeck))
+            .then((djangoStuff) => {
+                 resolve(djangoStuff);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+        });
+    };
+
+return {getCards, postCard, getDecks, postDeck};	
 
 
 
